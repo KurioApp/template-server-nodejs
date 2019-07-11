984b1cb6a7df2506a91397a825fa0b4edb40fc08
Rifad Ainun Nazieb <ainun@nazieb.com>
2019-07-11 18:12:25 +0700

    feat: configure logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import logger from "winston";
 import server from "./server";
 
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+logger.configure({
+  transports: new logger.transports.Console(),
+  format: isDev ? logger.format.simple() : logger.format.json(),
+  level: isDev ? 'silly' : 'info',
+});
+
+if (isDev) {
+  logger.warn('app runs in dev mode');
+}
+
 const port = process.env.APP_PORT || 3000;
 server.listen(port, () => {
   logger.info(`app runs on port ${port}`);
