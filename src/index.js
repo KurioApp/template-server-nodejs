2aff12639e081b8b94b7caa09863001ea9c5245d
Rifad Ainun Nazieb <ainun@nazieb.com>
2019-07-11 18:12:47 +0700

    feat: configure logs

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 logger.configure({
   transports: new logger.transports.Console(),
   format: isDev ? logger.format.simple() : logger.format.json(),
-  level: isDev ? 'silly' : 'info',
+  level: isDev ? "silly" : "info"
 });
 
 if (isDev) {
-  logger.warn('app runs in dev mode');
+  logger.warn("app runs in dev mode");
 }
 
 const port = process.env.APP_PORT || 3000;
