4ce8bd96a66ea7d06224579882d115ded13d6c73
ekapratama93 <ekapratama93@gmail.com>
2020-07-30 15:43:44 +0700

    enable prometheus middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import * as prometheus from "./prometheus";
 
+const metricRoute = process.env.PROMETHEUS_PATH || "/metrics";
 const server = express();
 
 server.disable("x-powered-by");
 server.get("/ping", (req, res) => res.send("pong"));
+server.use(
+  prometheus.middleware({
+    excludedRoutes: ["/ping", metricRoute]
+  })
+);
 
 export default server;
