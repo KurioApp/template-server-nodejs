f08d4e4b86141ca6cb240d10bb247f0b84b30389
ridhoperdana <ridhoperdana@ymail.com>
2020-10-23 12:21:09 +0700

    refactor: add boilerplate for service and repository layer

---
+++ src/entity.js
@@ -0,0 +1,25 @@
+// You should create a new file for each entity object
+// in this case, if we want to have an article object, you can rename this file
+// into article.js
+// Declare entity using Factory function.
+
+// function Article(id, title, content, publisherID) {
+//   return {
+//     id,
+//     title,
+//     content,
+//     publisher: {
+//       id: publisherID
+//     }
+//   };
+// }
+//
+// function Publisher(id, name, url) {
+//   return {
+//     id,
+//     name,
+//     url
+//   };
+// }
+//
+// export { Article, Publisher };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
+// import repository from "./repository/repository";
+// import service from "./service";
+// import MysqlORM from "./repository/mysql";
 
 const server = express();
+server.use(bodyParser.json());
+
+// Init repository and service before using the variable inside the handler.
+// included template is using Mysql and Sequelize as the ORM.
+// const Repo = repository(MysqlORM);
+// const Service = service(Repo);
 
 server.disable("x-powered-by");
 server.get("/ping", (req, res) => res.send("pong"));
